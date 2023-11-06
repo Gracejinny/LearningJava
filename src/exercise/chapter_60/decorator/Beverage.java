56270fccc2fe0b146dae691ff295908746fee093
Eunji Kim <eun_k_j@naver.com>
2023-11-06 20:21:52 +0900

    디자인 패턴

---
+++ src/exercise/chapter_60/decorator/Beverage.java
@@ -0,0 +1,7 @@
+package exercise.chapter_60.decorator;
+
+// Component Interface
+public interface Beverage {
+    String getDescription(); // 설명
+    double cost(); // 가격
+}
---
+++ src/exercise/chapter_60/strategy/SeasonDiscountStrategy.java
@@ -0,0 +1,9 @@
+package exercise.chapter_60.strategy;
+
+public class SeasonDiscountStrategy implements DiscountStrategy{
+
+    @Override
+    public double calculateDiscount(double amount) {
+        return amount * 0.1;
+    }
+}
